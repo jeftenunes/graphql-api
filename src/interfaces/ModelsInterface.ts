7273b71ef6c4c496386c91741401cc5b82db8273
Jéfte Santos <jeftesn22@gmail.com>
2017-12-11 23:36:02 -0300

    [feature/create-objective-model] - No patience.

--- src/interfaces/ModelsInterface.ts
+++ src/interfaces/ModelsInterface.ts
@@ -1,3 +1,7 @@
+import { UserModel } from "../models/UserModel";
+import { ObjectiveModel } from "../models/ObjectiveModel";
+
 export interface ModelsInterface {
-    
+    User: UserModel;
+    Objective: ObjectiveModel;
 }
