886303550cd6c2488e1441a5e3c082f17a05b6f5
Jéfte Santos <jeftesn22@gmail.com>
2017-12-18 22:45:10 -0300

    Removed ObjectiveModel

--- src/interfaces/ModelsInterface.ts
+++ src/interfaces/ModelsInterface.ts
@@ -1,7 +1,5 @@
 import { UserModel } from "../models/UserModel";
-import { ObjectiveModel } from "../models/ObjectiveModel";
 
 export interface ModelsInterface {
     User: UserModel;
-    Objective: ObjectiveModel;
 }
